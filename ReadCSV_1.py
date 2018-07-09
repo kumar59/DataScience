65692e2b69289cce1fbbb76bdad828abdb0464a3
kumar59 <kumar595@yahoo.com>
2018-07-09 16:03:21 -0400

    Add files via upload

---
+++ ReadCSV_1.py
@@ -0,0 +1,29 @@
+#%%
+"""
+read csv file
+"""
+#%%
+#---------#
+import unicodecsv
+
+enrollments = []
+f = open('enrollments.csv','rb')
+reader = unicodecsv.DictReader(f)
+
+for row in reader:
+    enrollments.append(row)
+
+
+f.close()
+
+len1 = len(enrollments)
+cnt=0
+while cnt < len1:
+    print(enrollments[cnt])
+    cnt=cnt+1
+
+#print('first line of array is',enrollments[0])
+#print('last line of array is',enrollments[len1])
+
+#--- This may need additional work--#
+#%%
