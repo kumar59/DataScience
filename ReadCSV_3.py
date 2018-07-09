d2404b0d4e18e26558e03d7d86883395bfb1fb3f
kumar59 <kumar595@yahoo.com>
2018-07-09 16:24:21 -0400

    Add files via upload

--- ReadCSV_3.py
+++ ReadCSV_3.py
@@ -6,11 +6,11 @@ read daily engagement csv file
 import unicodecsv
 
 project_submissions = []
-with open('project_submissions','rb') as f:
+with open('project_submissions.csv','rb') as f:
      reader = unicodecsv.DictReader(f)
      project_submissions=list(reader)
 
 print(' ')
 project_submissions[0]
 
-#%% 
+#%%
